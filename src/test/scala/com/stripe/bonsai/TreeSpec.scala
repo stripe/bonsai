a591102c4b72d1306ed5cc0168a55def17169691
Tom Switzer <thomas@stripe.com>
2015-10-08 14:35:42 -0400

    Remove some commented imports.

--- src/test/scala/com/stripe/bonsai/TreeSpec.scala
+++ src/test/scala/com/stripe/bonsai/TreeSpec.scala
@@ -3,9 +3,6 @@ package com.stripe.bonsai
 import org.scalatest.{ WordSpec, Matchers }
 import org.scalatest.prop.Checkers
 
-//import org.scalacheck.Arbitrary._
-//import org.scalacheck.Prop._
-
 class TreeSpec extends WordSpec with Matchers with Checkers {
   "Tree.apply" should {
     "copy structure of tree" in {
